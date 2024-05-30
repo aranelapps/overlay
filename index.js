b69eaad8ca118f4ac8a592e2ce55a7d173a22e53
aranelapps <kensawatzky@gmail.com>
2024-05-29 23:17:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+import { useRouter } from 'next/router';
 
+const Index = () => {
+  const router = useRouter();
+  const { query } = router;
+
+  const items = Object.values(query);
+
+  return (
+    <div style={overlayStyle}>
+      <h1>Overlay List</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const overlayStyle = {
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  color: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+export default Index;
